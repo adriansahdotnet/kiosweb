cb49b8823ad451ec786f48e89e2fa6ff1ea6b7d4
adriansahdotnet <ardhiyasa11@gmail.com>
2022-10-08 10:09:20 +0700

    update 1.5.6

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,19 +1,20 @@
 import Head from "next/head";
 
-const SEO = ({ pageTitle,font }) => (
+const SEO = ({ pageTitle, font }) => (
   <>
     <Head>
-      <title>
-       Kios Web | Jasa pembuatan website #No1 di indonesia.
-      </title>
+      <title>Kios Web | Jasa pembuatan website #No1 di indonesia.</title>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-      <meta name="description" content="Jasa pembuatan website #No1 Indonesia." />
-      <meta name="robots" content="noindex, follow" />
+      <meta
+        name="description"
+        content="Jasa pembuatan website #No1 Indonesia."
+      />
+      <meta name="robots" content="index, follow" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-      
+
       {font && <link href={font} rel="stylesheet" />}
       <link rel="icon" href="/faviconu.ico" />
     </Head>
