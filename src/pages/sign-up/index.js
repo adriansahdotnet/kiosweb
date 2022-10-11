c6582216cfe31ae5b0fd4a44362e5d37138e1a8d
adriansahdotnet <ardhiyasa11@gmail.com>
2022-10-11 10:44:30 +0700

    update seo

--- src/pages/sign-up/index.js
+++ src/pages/sign-up/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import BgShape from '../../components/common/BgShape';
-import Footer from '../../components/Home/Footer';
-import Header from '../../components/Home/Header';
-import SEO from '../../components/seo';
-import SignUp from '../../components/SignUp/SignUp';
+import React from "react";
+import BgShape from "../../components/common/BgShape";
+import Footer from "../../components/Home/Footer";
+import Header from "../../components/Home/Header";
+import SEO from "../../components/seo";
+import SignUp from "../../components/SignUp/SignUp";
 
 const Register = () => {
-    return (
-        <>
-            <SEO pageTitle={'Sign Up'} />
-            <Header />
-            <BgShape />
-            <SignUp />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <SEO pageTitle={"Daftar"} />
+      <Header />
+      <BgShape />
+      <SignUp />
+      <Footer />
+    </>
+  );
 };
 
 export default Register;
