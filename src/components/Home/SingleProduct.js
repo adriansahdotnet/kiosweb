7b8f053d9ea41d0edc4bfb3a5373c8f2a2129795
adriansahdotnet <ardhiyasa11@gmail.com>
2022-10-11 10:40:28 +0700

    update tag

--- src/components/Home/SingleProduct.js
+++ src/components/Home/SingleProduct.js
@@ -40,8 +40,8 @@ const SingleProduct = ({ product }) => {
           </div>
           <div className="product__content">
             <div className="product__meta mb-10 d-flex justify-content-between align-items-center">
-              <div className="product__tag">
-                <a href="#">{category}</a>
+              <div>
+                <h4>Harga</h4>
               </div>
               <div className="product__price">
                 <span>{`RP. ${price}`}</span>
@@ -53,7 +53,7 @@ const SingleProduct = ({ product }) => {
               </Link>
             </h3>
             <p className="product__author">
-              by <a href="#">Kiosweb</a> in <a href="#">Jasa web</a>
+              by <a href="#">Kiosweb</a> di <a href="#">Jasa web</a>
             </p>
           </div>
         </div>
