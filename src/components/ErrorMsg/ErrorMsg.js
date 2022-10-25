98432199c2ac139e4d25906110e77300040400cf
adriansahdotnet <ardhiyasa11@gmail.com>
2022-10-25 08:50:37 +0700

    remove

--- src/components/ErrorMsg/ErrorMsg.js
+++ src/components/ErrorMsg/ErrorMsg.js
@@ -1,16 +1,16 @@
-import Link from 'next/link';
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import Link from "next/link";
+import React from "react";
+import { Container } from "react-bootstrap";
 
-const ErrorMsg = ({ errTitle = "Your cart is currently empty." }) => {
-    return (
-        <Container className='pb-80'>
-            <h1 className='mb-40 text-capitalize'>{errTitle}</h1>
-            <Link href="/product">
-                <a className='m-btn m-btn-2'>Kembali kek product.</a>
-            </Link>
-        </Container>
-    );
+const ErrorMsg = ({ errTitle = "Halaman tidak ditemukan." }) => {
+  return (
+    <Container className="pb-80">
+      <h1 className="mb-40 text-capitalize">{errTitle}</h1>
+      <Link href="/product">
+        <a className="m-btn m-btn-2">Kembali ke product.</a>
+      </Link>
+    </Container>
+  );
 };
 
 export default ErrorMsg;
