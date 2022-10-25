169707b6a27b7416c37965e02d68b552625839b5
adriansahdotnet <ardhiyasa11@gmail.com>
2022-10-25 08:45:37 +0700

    remove

--- src/pages/support/index.js
+++ src/pages/support/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Footer from "../../components/Home/Footer";
 import Header from "../../components/Home/Header";
 import SEO from "../../components/seo";
-import BlogArea from "../../components/support/BlogArea";
 import CtaArea from "../../components/support/CtaArea";
 import FaqArea from "../../components/support/FaqArea";
 import SupportArea from "../../components/support/SupportArea";
