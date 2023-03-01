fea25ae05c91e8246ac93fe0731a820cb9beaa5b
kioswebid <ardhiyasa11@gmail.com>
2023-03-01 18:37:09 +0700

    add gamesk

--- src/pages/gamesk/index.js
+++ src/pages/gamesk/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import BgShape from "../../components/common/BgShape";
 import Footer from "../../components/Home/Footer";
 import Header from "../../components/Home/Header";
-import ProductDetailsArea from "../../components/cgames-v2/ProductDetailsArea";
+import ProductDetailsArea from "../../components/gamesk/ProductDetailsArea";
 import ProductTitle from "../../components/gamesk/ProductTitle";
 import SEO from "../../components/seo";
 
